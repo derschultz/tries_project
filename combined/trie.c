b71ae7cc96e36712e421c039a2b2ca64ad051fa3
schultz <schultz@localhost>
2014-12-23 13:53:08 -0500

    Change output to csv format, add time measurements for each task

--- combined/trie.c
+++ combined/trie.c
@@ -472,6 +472,30 @@ int trie_delete(Trie *t, void *item, int sz){
 int trie_search(Trie *t, void *item, int sz){
     return trie_traverse(t, item, sz, SEARCH);
 }
+
+#define NSEC_PER_SEC 1000000000
+
+//given two times, start and end, calculate the elapsed time from start to end and store it in delta
+//taken with modifications from http://www.guyrutenberg.com/2007/09/22/profiling-code-using-clock_gettime/
+//Since we're using CLOCK_MONOTONIC below, we don't need to worry about negative times
+void timespec_diff(struct timespec *start, struct timespec *end, struct timespec *delta){
+    if((end->tv_nsec - start->tv_nsec) < 0){
+        delta->tv_sec = end->tv_sec - start->tv_sec - 1;
+        delta->tv_nsec = NSEC_PER_SEC + end->tv_nsec - start->tv_nsec;
+    } else {
+        delta->tv_sec = end->tv_sec - start->tv_sec;
+        delta->tv_nsec = end->tv_nsec - start->tv_nsec;
+    }
+}
+
+//given a time t and a divisor d, put into delta the result of dividing the time t into d parts.
+void timespec_div(struct timespec *t, int d, struct timespec *delta){
+    uint64_t total = (NSEC_PER_SEC * t->tv_sec) + t->tv_nsec;
+    total /= d;
+    delta->tv_sec = total / NSEC_PER_SEC;
+    delta->tv_nsec = total % NSEC_PER_SEC; 
+}
+
 int main(int argc, char** argv){
     if(argc != 5){
         printf("Usage: ./trie <count> <integer size in bytes> <input file> <trie type [1-4]>\n");
@@ -506,16 +530,31 @@ int main(int argc, char** argv){
         printf("Error opening %s for reading.\n", argv[1]);
         exit(-2);
     }
-    printf("reading %d integers of size %d bytes from %s...",count, len, argv[3]);
+    //printf("reading %d integers of size %d bytes from %s...",count, len, argv[3]);
     for(i = 0; i < count; i += len){
-        read(FD, (void*) &(randoms[i]), len);
+        if(read(FD, (void*) &(randoms[i]), len) != len){
+            printf("read() failed! exiting...\n");
+            exit(-3);
+        }
     }
     close(FD);
-    printf("done.\noperating on trie of type %d...",type);
+    //printf("done.\noperating on trie of type %d...",type);
 
-    struct timespec ts_before, ts_after;
+    //Here's what we do:
+    //for each integer:
+    //  attempt to insert the integer, make sure it works by inspecting the return code
+    //  attempt to insert the integer again, make sure it fails this time (as that integer was already inserted)
+    //  search for the integer, make sure it exists in the trie by inspecting the return code
+    //  delete the integer, make sure the deletion works by inspecting the return code
+    //  search for the integer, make sure it doesn't exist, as it just got deleted
+    //  delete the integer again, make sure this delete fails since the element is already gone.
+    //This all assumes that the set of integers we store has no duplicates, as done by the generation process in the included python script.
+    printf("%d,%d,%d,", count, len, type);
+    struct timespec ts_before, ts_after, delta, divided;
     memset(&ts_before, 0, sizeof(struct timespec));
     memset(&ts_after, 0, sizeof(struct timespec));
+    memset(&delta, 0, sizeof(struct timespec));
+    memset(&divided, 0, sizeof(struct timespec));
 
     clock_gettime(CLOCK_MONOTONIC, &ts_before);
     //printf("inserting into trie %d...", j);
@@ -525,7 +564,29 @@ int main(int argc, char** argv){
         }
     }
     //printf("done.\n");
+    clock_gettime(CLOCK_MONOTONIC, &ts_after);
+    timespec_diff(&ts_before, &ts_after, &delta);
+    timespec_div(&delta, count, &divided);
+    printf("%li.%09u,%li.%09u,", 
+           delta.tv_sec, delta.tv_nsec, 
+           divided.tv_sec, divided.tv_nsec);
+    
+    clock_gettime(CLOCK_MONOTONIC, &ts_before);
+    //printf("checking insert into trie %d...", j);
+    for(i = 0; i < count; i += len){
+        if(trie_insert(t, (void*) &(randoms[i]), len)){
+            printf("error inserting %u into trie after it was already inserted!\n", randoms[i]);
+        }
+    }
+    //printf("done.\n");
+    clock_gettime(CLOCK_MONOTONIC, &ts_after); 
+    timespec_diff(&ts_before, &ts_after, &delta);
+    timespec_div(&delta, count, &divided);
+    printf("%li.%09u,%li.%09u,", 
+           delta.tv_sec, delta.tv_nsec, 
+           divided.tv_sec, divided.tv_nsec);
 
+    clock_gettime(CLOCK_MONOTONIC, &ts_before);
     //printf("searching in trie %d...", j);
     for(i = 0; i < count; i += len){
         if(!trie_search(t, (void*) &(randoms[i]), len)){
@@ -533,7 +594,14 @@ int main(int argc, char** argv){
         }
     }
     //printf("done.\n");
-    
+    clock_gettime(CLOCK_MONOTONIC, &ts_after); 
+    timespec_diff(&ts_before, &ts_after, &delta);
+    timespec_div(&delta, count, &divided);
+    printf("%li.%09u,%li.%09u,", 
+           delta.tv_sec, delta.tv_nsec, 
+           divided.tv_sec, divided.tv_nsec);
+   
+    clock_gettime(CLOCK_MONOTONIC, &ts_before);
     //printf("deleting in trie %d...", j);
     for(i = 0; i < count; i += len){
         if(!trie_delete(t, (void*) &(randoms[i]), len)){
@@ -541,7 +609,29 @@ int main(int argc, char** argv){
         }
     }
     //printf("done.\n");
-
+    clock_gettime(CLOCK_MONOTONIC, &ts_after); 
+    timespec_diff(&ts_before, &ts_after, &delta);
+    timespec_div(&delta, count, &divided);
+    printf("%li.%09u,%li.%09u,", 
+           delta.tv_sec, delta.tv_nsec, 
+           divided.tv_sec, divided.tv_nsec);
+   
+    clock_gettime(CLOCK_MONOTONIC, &ts_before);
+    //printf("checking delete in trie %d...", j);
+    for(i = 0; i < count; i += len){
+        if(trie_delete(t, (void*) &(randoms[i]), len)){
+            printf("error deleting %u from trie after it was already deleted!!\n", randoms[i]);
+        }
+    }
+    //printf("done.\n");
+    clock_gettime(CLOCK_MONOTONIC, &ts_after); 
+    timespec_diff(&ts_before, &ts_after, &delta);
+    timespec_div(&delta, count, &divided);
+    printf("%li.%09u,%li.%09u,", 
+           delta.tv_sec, delta.tv_nsec, 
+           divided.tv_sec, divided.tv_nsec);
+   
+    clock_gettime(CLOCK_MONOTONIC, &ts_before);
     //printf("making sure delete worked in trie %d...", j);
     for(i = 0; i < count; i += len){
         if(trie_search(t, (void*) &(randoms[i]), len)){
@@ -550,11 +640,24 @@ int main(int argc, char** argv){
     }
     //printf("done.\n");
     clock_gettime(CLOCK_MONOTONIC, &ts_after); 
-    printf("done.\n");
+    timespec_diff(&ts_before, &ts_after, &delta);
+    timespec_div(&delta, count, &divided);
+    printf("%li.%09u,%li.%09u,", 
+           delta.tv_sec, delta.tv_nsec, 
+           divided.tv_sec, divided.tv_nsec);
+   //printf("done.\n");
+    /*
     printf("time delta: %li seconds, %u nanoseconds\n", 
             ts_after.tv_sec - ts_before.tv_sec,
             abs(ts_after.tv_nsec - ts_before.tv_nsec));
-
-    memset(&ts_before, 0, sizeof(struct timespec));
-    memset(&ts_after, 0, sizeof(struct timespec));
+    */
+    //unsigned long long delta = 1000000000 * (ts_after.tv_sec - ts_before.tv_sec) + abs(ts_after.tv_nsec - ts_before.tv_nsec);
+    /*printf("%d,%d,%d,%li.%09u\n", 
+            count, len, type, 
+            ts_after.tv_sec - ts_before.tv_sec,
+            abs(ts_after.tv_nsec - ts_before.tv_nsec));*/
+    //printf("%d,%d,%d,%llu\n", count, len, type, delta);
+    //memset(&ts_before, 0, sizeof(struct timespec));
+    //memset(&ts_after, 0, sizeof(struct timespec));
+    printf("\n");
 }
